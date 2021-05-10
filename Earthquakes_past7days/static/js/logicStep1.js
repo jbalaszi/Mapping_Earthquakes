be7b5c566984a52b6be351651697b2f00249e7a6
jbalaszi <balaszij@yahoo.com>
2021-05-10 14:29:21 -0500

    files

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,32 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at Toronto.
-//let map = L.map('mapid').setView([44.0, -80.0], 2);
-
-// Add GeoJSON
-//let sanFranAirport =
-// {"type":"FeatureCollection","features":[{
-   // "type":"Feature",
-    //"properties":{
-      //  "id":"3469",
-       // "name":"San Francisco International Airport",
-        //"city":"San Francisco",
-        //"country":"United States",
-        //"faa":"SFO",
-        //"icao":"KSFO",
-        //"alt":"13",
-        //"tz-offset":"-8",
-        //"dst":"A",
-        //"tz":"America/Los_Angeles"},
-        //"geometry":{
-          //  "type":"Point",
-            //"coordinates":[-122.375,37.61899948120117]}}
-//]};
-
-// Create the map object with center at the San Francisco airport.
-//let map = L.map('mapid').setView([30, 30], 2);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -49,10 +23,10 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer. 
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satelliteStreets]
-})
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -61,8 +35,7 @@ L.control.layers(baseMaps).addTo(map);
 let torontoHoods = "https://raw.githubusercontent.com/jbalaszi/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
 });
